345653a3689ec57f35574b9f75033bf27d9d32dc
tattedweazel <tattedweazel@gmail.com>
2013-10-19 23:27:35 -0700

    Getting to the meat

--- content/home.content.php
+++ content/home.content.php
@@ -2,4 +2,26 @@
 $page = $red->page;
 ?>
 
-<h1>Home Page</h1>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3 text-center">
+			welcome.to.trust.or.betray - make.your.selection.below
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="jumbotron">
+			  <div class="container">
+			  	<div class="row text-center">
+			  		<div class="col-md-6">
+			    		<?php echo $page->linkTo(array('href' => '', 'class' => 'btn btn-primary btn-lg'), 'create.game');?>
+			    	</div>
+		    		<div class="col-md-6">
+			    		<?php echo $page->linkTo(array('href' => '', 'class' => 'btn btn-primary btn-lg'), 'join.game');?>
+			    	</div>
+			    </div>
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
