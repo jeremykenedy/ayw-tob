dec1f5017d66479b5eb080b9b6c84c162c8f993d
tattedweazel <tattedweazel@gmail.com>
2013-10-20 00:02:24 -0700

    Initial menus

--- content/home.content.php
+++ content/home.content.php
@@ -14,10 +14,10 @@ $page = $red->page;
 			  <div class="container">
 			  	<div class="row text-center">
 			  		<div class="col-xs-6">
-			    		<?php echo $page->linkTo(array('href' => '', 'class' => 'btn btn-primary btn-lg'), 'create.game');?>
+			    		<?php echo $page->linkTo(array("data-toggle"=> "modal", "href" => "#create_game_modal", 'class' => 'btn btn-primary btn-lg'), 'create.game');?>
 			    	</div>
 		    		<div class="col-xs-6">
-			    		<?php echo $page->linkTo(array('href' => '', 'class' => 'btn btn-primary btn-lg'), 'join.game');?>
+			    		<?php echo $page->linkTo(array("data-toggle"=> "modal", "href" => "#join_game_modal", 'class' => 'btn btn-primary btn-lg'), 'join.game');?>
 			    	</div>
 			    </div>
 			  </div>
@@ -25,3 +25,32 @@ $page = $red->page;
 		</div>
 	</div>
 </div>
+
+<div class="modal fade" id="create_game_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form id="create_game_form">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title">create.game</h4>
+        </div>
+        <div class="modal-body">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-3 col-xs-12">
+                name.this.game
+              </div>
+              <div class="col-md-9 col-xs-12">
+                <?php echo $page->formInput('text', 'name', '', array('class' => 'required', 'data-req' => 'exists', 'autocomplete' => 'off'));?>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">close</button>
+          <button id="reset_button" type="button" class="btn btn-primary">reset.password</button>
+        </div>
+      </form>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
---
+++ models/game.model.php
@@ -0,0 +1,81 @@
+<?php
+class Game extends Database{
+	
+	const ALL_FIELDS = "id, code, name, created, status, owner_fk, public";
+
+	public function getAll(){
+		$sql = "
+				SELECT 
+					".$this::ALL_FIELDS."
+				 FROM games
+		";
+		$results = $this->query($sql);
+		return  $this->processResults($results);
+	}
+
+	public function getJoinable(){
+		$sql = "
+				SELECT 
+					".$this::ALL_FIELDS."
+				 FROM 
+				 	games
+				 WHERE
+				 	status = 'waiting'
+			 	AND
+			 		public = 1
+		";
+		$results = $this->query($sql);
+		return  $this->processResults($results);
+	}
+
+	public function getById($id){
+		$sql = "
+				SELECT 
+					".$this::ALL_FIELDS."
+				 FROM 
+				 	games
+			 	WHERE 
+			 		id = ?
+		";
+		$results = $this->query($sql, array($id));
+		return  $this->processResults($results);
+	}
+
+	public function getByCode($code){
+		$sql = "
+				SELECT 
+					".$this::ALL_FIELDS."
+				 FROM 
+				 	games
+			 	WHERE 
+			 		code = ?
+		";
+		$results = $this->query($sql, array($code));
+		return  $this->processResults($results);
+	}
+
+	public function getByOwner($userId){
+		$sql = "
+				SELECT 
+					".$this::ALL_FIELDS."
+				 FROM 
+				 	games
+			 	WHERE 
+			 		owner_fk = ?
+		";
+		$results = $this->query($sql, array($userId));
+		return  $this->processResults($results);
+	}
+
+	public function createNewGame($code, $name, $created, $status, $owner_fk, $public){
+		$sql = "
+				INSERT INTO
+					games
+						(code, name, created, status, owner_fk, public)
+				VALUES
+					(?, ?, ?, ?, ?, ?)
+		";
+		$success = $this->execute($sql, array($code, $name, $created, $status, $owner_fk, $public));
+		return $success;
+	}
+}
